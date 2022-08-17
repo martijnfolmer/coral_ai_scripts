7837ec2f6efb80ee82792e6b45d009a02426f486
martijnfolmer <31698991+martijnfolmer@users.noreply.github.com>
2022-08-17 15:21:41 +0200

    Update hello_world.py

--- hello_world.py
+++ hello_world.py
@@ -1,1 +1,10 @@
+import argparse
+
+from PIL import Image
+from PIL import ImageDraw
+from pycoral.adapters import common
+from pycoral.utils.edgetpu import make_interpreter
+
+
+print("We succesfully imported new stuff")
 print("Hello World!")
